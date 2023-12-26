8a32f42ef4ed6cb82232bd7e1cf6ca78a85b12b2
= <66930763+tyler-lutz@users.noreply.github.com>
2023-12-26 15:00:13 -0500

    add getURLsFromHTML and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require('jsdom')
+
 function normalizeURL(url) {
     const urlObj = new URL(url)
     let normalizedURL = `${urlObj.hostname}${urlObj.pathname}`
@@ -7,6 +9,21 @@ function normalizeURL(url) {
     return normalizedURL
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const links = dom.window.document.querySelectorAll('a')
+    links.forEach(link => {
+        const href = link.getAttribute('href')
+        if (href) {
+            const url = new URL(href, baseURL)
+            urls.push(url.toString())
+        }
+    })
+    return urls
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 
 test('normalizeURL', () => {
     const input = 'https://blog.boot.dev/path'
@@ -26,5 +26,21 @@ test('normalizeURL http', () => {
     const input = 'http://blog.boot.dev/path'
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absolute path', () => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody = '<html><body><a href="https://blog.boot.dev">Boot.dev</a></body></html>'
+    const actual = getURLsFromHTML(inputBody, inputURL)
+    const expected = ['https://blog.boot.dev/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative path', () => {
+    const inputURL = 'https://blog.boot.dev'
+    const inputBody = '<html><body><a href="/about">About</a></body></html>'
+    const actual = getURLsFromHTML(inputBody, inputURL)
+    const expected = ['https://blog.boot.dev/about']
     expect(actual).toEqual(expected)
 })
